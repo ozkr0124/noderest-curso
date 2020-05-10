42e5365f588a033c3cfa3a6f98544011cb3856ac
ozkr0124 <oloaiza.une@gmail.com>
2020-05-10 14:17:59 -0500

    REST Server

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,13 @@
 process.env.PORT = process.env.PORT || 3000;
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/curso';
+} else {
+    urlDB = 'mongodb://cursos:abcd1234@ds263068.mlab.com:63068/cursoozkr0124';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,45 +1,30 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
-// parse application/x-www-form-urlencoded
+
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
-// parse application/json
-app.use(bodyParser.json())
+app.use(require('./routes/usuario'));
 
-
-
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
-})
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
+let conn = async() => {
+    let db = await mongoose.connect(process.env.URLDB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     });
-});
 
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
-})
+    return db;
+}
+
+conn().then(res => {
+    console.log('Conexion DB ONLINE');
+}).catch(e => {
+    console.log('Error en la conexion', e);
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Puerto ${process.env.PORT}`);
